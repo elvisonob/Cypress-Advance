4447c156aa0b754b30ff4cf3d41b81e49aa8d922
elvisonob <elvisonob@yahoo.com>
2023-09-15 05:03:02 +0100

    using Cypress Aliases

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -19,4 +19,36 @@ describe('contact form', () => {
     cy.get('@submitBtn').contains('Sending...');
     cy.get('@submitBtn').should('have.attr', 'disabled');
   });
+
+  it('should validate the form input', () => {
+    cy.visit('http://localhost:5173/about');
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr('disabled');
+      expect(el.text()).to.not.equal('Sending...');
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+    cy.get('[data-cy="contact-input-message"]').as('msgInput');
+    cy.get('@msgInput').focus().blur();
+    cy.get('@msgInput')
+      .parent()
+      .then((el) => {
+        expect(el.attr('class')).to.contains('invalid');
+      });
+
+    cy.get('[data-cy="contact-input-name"]').as('nameInput');
+    cy.get('@nameInput').focus().blur();
+    cy.get('@nameInput')
+      .parent()
+      .then((el) => {
+        expect(el.attr('class')).to.contains('invalid');
+      });
+    cy.get('[data-cy="contact-input-email"]').as('emailInput');
+    cy.get('@emailInput').focus().blur();
+    cy.get('@emailInput')
+      .parent()
+      .then((el) => {
+        expect(el.attr('class')).to.contains('invalid');
+      });
+  });
 });
